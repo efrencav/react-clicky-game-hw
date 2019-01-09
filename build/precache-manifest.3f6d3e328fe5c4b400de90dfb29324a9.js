7adf2abd063f1946861e74af002254763319c7d1
efrencav <efren@abstract-creative.com>
2019-01-08 21:44:03 -0600

    build package

---
+++ build/precache-manifest.3f6d3e328fe5c4b400de90dfb29324a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c55074010a16da22c99",
+    "url": "/static/css/main.5dd0d3c3.chunk.css"
+  },
+  {
+    "revision": "3c55074010a16da22c99",
+    "url": "/static/js/main.3c550740.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "343518182904d6765dabe96c4d16e09c",
+    "url": "/index.html"
+  }
+];
